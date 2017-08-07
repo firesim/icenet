c3fd2fece72e23e7faa701e8d0154380c69c785d
Howard Mao <zhehao.mao@gmail.com>
2017-08-07 20:23:01 +0000

    add missing etherType field to header

--- src/main/scala/Consts.scala
+++ src/main/scala/Consts.scala
@@ -8,10 +8,13 @@ object IceNetConsts {
   val ETH_MAX_WORDS = 190
   val ETH_HEAD_WORDS = 2
   val ETH_MAC_BITS = 48
+  val ETH_TYPE_BITS = 16
+  val ETH_PAD_BITS = 16
 }
 
 class EthernetHeader extends Bundle {
+  val ethType = UInt(IceNetConsts.ETH_TYPE_BITS.W)
   val srcmac = UInt(IceNetConsts.ETH_MAC_BITS.W)
   val dstmac = UInt(IceNetConsts.ETH_MAC_BITS.W)
-  val padding = UInt(16.W)
+  val padding = UInt(IceNetConsts.ETH_PAD_BITS.W)
 }
