39cd93e3f6352a7b95304d9102d9df08d301a327
Howard Mao <zhehao.mao@gmail.com>
2017-11-28 16:00:24 -0800

    simplify NetworkTap

--- src/main/scala/Tap.scala
+++ src/main/scala/Tap.scala
@@ -19,7 +19,6 @@ class NetworkTap[T <: Data](
 
   val io = IO(new Bundle {
     val inflow = Flipped(Decoupled(new StreamChannel(wordBits)))
-    val backflow = Flipped(Decoupled(new StreamChannel(wordBits)))
     val passthru = Decoupled(new StreamChannel(wordBits))
     val tapout = Decoupled(new StreamChannel(wordBits))
   })
@@ -34,8 +33,7 @@ class NetworkTap[T <: Data](
 
   val (s_inflow_header :: s_check_header ::
        s_passthru_header :: s_passthru_body ::
-       s_tapout_body :: s_check_backflow ::
-       s_backflow_header :: s_backflow_body :: Nil) = Enum(8)
+       s_tapout_header :: s_tapout_body :: Nil) = Enum(6)
   val state = RegInit(s_inflow_header)
 
   io.inflow.ready := MuxLookup(state, false.B, Seq(
@@ -43,38 +41,41 @@ class NetworkTap[T <: Data](
     s_passthru_body -> io.passthru.ready,
     s_tapout_body -> io.tapout.ready))
 
-  io.backflow.ready := state === s_backflow_body && io.passthru.ready
-
   io.passthru.valid := MuxLookup(state, false.B, Seq(
     s_passthru_header -> true.B,
-    s_passthru_body -> io.inflow.valid,
-    s_backflow_header -> true.B,
-    s_backflow_body -> io.backflow.valid))
+    s_passthru_body -> io.inflow.valid))
   io.passthru.bits.data := MuxLookup(state, 0.U, Seq(
     s_passthru_header -> headerVec(headerIdx),
-    s_passthru_body -> io.inflow.bits.data,
-    s_backflow_header -> headerVec(headerIdx),
-    s_backflow_body -> io.backflow.bits.data))
+    s_passthru_body -> io.inflow.bits.data))
   io.passthru.bits.last := MuxLookup(state, false.B, Seq(
     s_passthru_header -> (bodyLess && headerIdx === headerLen),
-    s_passthru_body -> io.inflow.bits.last,
-    s_backflow_body -> io.backflow.bits.last))
+    s_passthru_body -> io.inflow.bits.last))
   io.passthru.bits.keep := MuxLookup(state, ~0.U(wordBytes.W), Seq(
-    s_passthru_body -> io.inflow.bits.keep,
-    s_backflow_body -> io.backflow.bits.keep))
+    s_passthru_body -> io.inflow.bits.keep))
 
-  io.tapout.valid := state === s_tapout_body && io.inflow.valid
-  io.tapout.bits := io.inflow.bits
+  io.tapout.valid := MuxLookup(state, false.B, Seq(
+    s_tapout_header -> true.B,
+    s_tapout_body -> io.inflow.valid))
+  io.tapout.bits.data := MuxLookup(state, 0.U, Seq(
+    s_tapout_header -> headerVec(headerIdx),
+    s_tapout_body -> io.inflow.bits.data))
+  io.tapout.bits.last := MuxLookup(state, false.B, Seq(
+    s_tapout_header -> (bodyLess && headerIdx === headerLen),
+    s_tapout_body -> io.inflow.bits.last))
+  io.tapout.bits.keep := MuxLookup(state, ~0.U(wordBytes.W), Seq(
+    s_tapout_body -> io.inflow.bits.keep))
 
   when (state === s_inflow_header && io.inflow.valid) {
     headerIdx := headerIdx + 1.U
     headerVec(headerIdx) := io.inflow.bits.data
 
-    when (io.inflow.bits.last || headerIdx === (headerWords-1).U) {
+    val headerLast = headerIdx === (headerWords-1).U
+
+    when (io.inflow.bits.last || headerLast) {
       headerLen := headerIdx
       headerIdx := 0.U
       bodyLess := io.inflow.bits.last
-      state := Mux(io.inflow.bits.last, s_passthru_header, s_check_header)
+      state := Mux(headerLast, s_check_header, s_passthru_header)
     }
 
     assert(io.inflow.bits.keep.andR,
@@ -82,116 +83,58 @@ class NetworkTap[T <: Data](
   }
 
   when (state === s_check_header) {
-    state := Mux(selectFunc(header), s_tapout_body, s_passthru_header)
+    state := Mux(selectFunc(header), s_tapout_header, s_passthru_header)
   }
 
-  when (io.passthru.fire()) {
-    when (state.isOneOf(s_passthru_header, s_backflow_header)) {
-      headerIdx := headerIdx + 1.U
-      when (headerIdx === headerLen) {
-        headerIdx := 0.U
-        state := Mux(state === s_passthru_header,
-          s_passthru_body, s_backflow_body)
-      }
+  val header_fire = state === s_passthru_header && io.passthru.ready ||
+                    state === s_tapout_header   && io.tapout.ready
+  val body_fire = state.isOneOf(s_passthru_body, s_tapout_body) && io.inflow.fire()
+
+  when (header_fire) {
+    headerIdx := headerIdx + 1.U
+    when (headerIdx === headerLen) {
+      headerIdx := 0.U
+      state := Mux(bodyLess, s_inflow_header,
+        Mux(state === s_passthru_header, s_passthru_body, s_tapout_body))
     }
-    when (state.isOneOf(s_passthru_body, s_backflow_body)) {
-      when (io.passthru.bits.last) { state := s_inflow_header }
-    }
   }
 
-  when (io.tapout.fire() && io.tapout.bits.last) {
-    state := s_check_backflow
-  }
-
-  when (state === s_check_backflow) {
-    state := Mux(io.backflow.valid, s_backflow_header, s_inflow_header)
-  }
+  when (body_fire && io.inflow.bits.last) { state := s_inflow_header }
 }
 
 class NetworkTapTest extends UnitTest {
-  val ifWidth = 32
-  val sendHeaders = Seq(
-    EthernetHeader(1.U, 2.U, 0x800.U),
-    EthernetHeader(1.U, 3.U, 0x800.U),
-    EthernetHeader(1.U, 4.U, 0x801.U))
-  val sendBodies = Seq(
-    Seq(23.U, 13.U, 56.U, 12.U),
-    Seq(),
-    Seq(22.U, 16.U))
-  val sendPayloads = sendHeaders.zip(sendBodies).map {
-    case (header, body) => Vec(header.toWords(ifWidth) ++ body)
+  val sendPayloads = Seq(
+    Seq(0L, 0x800L << 48, 23L, 13L, 56L, 12L),
+    Seq(0L, 0x800L << 48),
+    Seq(0L, 0x801L << 48, 22, 16),
+    Seq(0L))
+  val sendLengths = sendPayloads.map(_.size)
+  val sendData = sendPayloads.flatten.map(l => BigInt(l))
+
+  val genIn = Module(new PacketGen(sendLengths, sendData))
+  genIn.io.start := io.start
+
+  val tapPayloads = sendPayloads.take(2)
+  val tapData = tapPayloads.flatten.map(l => BigInt(l))
+  val tapKeep = Seq.fill(tapData.size) { 0xff }
+  val tapLast = tapPayloads.flatMap {
+    pl => (Seq.fill(pl.size-1) { false } ++ Seq(true))
   }
+  val checkTap = Module(new PacketCheck(tapData, tapKeep, tapLast))
 
-  val sizes = sendPayloads.map(_.size)
-  val maxSize = sizes.reduce(max(_, _))
-  val nPhases = sizes.size
-
-  val sendPhase = RegInit(0.U(log2Ceil(nPhases).W))
-  val sendIdx = RegInit(0.U(log2Ceil(maxSize).W))
-  val sendData = Vec(sendPayloads.map(pl => pl(sendIdx)))(sendPhase)
-  val sendLast = Vec(sizes.map(sz => sendIdx === (sz-1).U))(sendPhase)
-  val sending = RegInit(false.B)
+  val passPayloads = sendPayloads.drop(2)
+  val passData = passPayloads.flatten.map(l => BigInt(l))
+  val passKeep = Seq.fill(passData.size) { 0xff }
+  val passLast = passPayloads.flatMap {
+    pl => (Seq.fill(pl.size-1) { false } ++ Seq(true))
+  }
+  val checkPass = Module(new PacketCheck(passData, passKeep, passLast))
 
   val tap = Module(new NetworkTap(
-    (header: EthernetHeader) => header.ethType === 0x800.U,
-    wordBytes = ifWidth / 8))
-  tap.io.inflow.valid := sending
-  tap.io.inflow.bits.data := sendData
-  tap.io.inflow.bits.last := sendLast
-  tap.io.inflow.bits.keep := ~0.U((ifWidth/8).W)
+    (header: EthernetHeader) => header.ethType === 0x800.U))
+  tap.io.inflow <> genIn.io.out
+  checkTap.io.in <> tap.io.tapout
+  checkPass.io.in <> tap.io.passthru
 
-  val backQueue = Module(new Queue(new StreamChannel(ifWidth), maxSize))
-  backQueue.io.enq <> tap.io.tapout
-  tap.io.backflow.valid := backQueue.io.deq.valid
-  tap.io.backflow.bits.data := backQueue.io.deq.bits.data + 1.U
-  tap.io.backflow.bits.last := backQueue.io.deq.bits.last
-  tap.io.backflow.bits.keep := backQueue.io.deq.bits.keep
-  backQueue.io.deq.ready := tap.io.backflow.ready
-
-  val recvBodies = sendBodies.head.map(_ + 1.U) +: sendBodies.tail
-  val recvPayloads = sendHeaders.zip(recvBodies).map {
-    case (header, body) => Vec(header.toWords(ifWidth) ++ body)
-  }
-
-  val recvPhase = RegInit(0.U(log2Ceil(nPhases).W))
-  val recvIdx = RegInit(0.U(log2Ceil(maxSize).W))
-  val recvData = Vec(recvPayloads.map(pl => pl(recvIdx)))(recvPhase)
-  val recvLast = Vec(sizes.map(sz => recvIdx === (sz-1).U))(recvPhase)
-  val receiving = RegInit(false.B)
-
-  tap.io.passthru.ready := receiving
-
-  val started = RegInit(false.B)
-
-  when (!started && io.start) {
-    started := true.B
-    sending := true.B
-    receiving := true.B
-  }
-
-  when (tap.io.inflow.fire()) {
-    sendIdx := sendIdx + 1.U
-    when (sendLast) {
-      sendIdx := 0.U
-      sendPhase := sendPhase + 1.U
-      when (sendPhase === (nPhases - 1).U) { sending := false.B }
-    }
-  }
-
-  when (tap.io.passthru.fire()) {
-    recvIdx := recvIdx + 1.U
-    when (recvLast) {
-      recvIdx := 0.U
-      recvPhase := recvPhase + 1.U
-      when (recvPhase === (nPhases - 1).U) { receiving := false.B }
-    }
-  }
-
-  io.finished := started && !(sending || receiving)
-
-  assert(!tap.io.passthru.valid ||
-         (tap.io.passthru.bits.data === recvData &&
-          tap.io.passthru.bits.last === recvLast &&
-          tap.io.passthru.bits.keep.andR),
-      "NetworkTapTest: received data, last, or keep incorrect")
+  io.finished := checkTap.io.finished || checkPass.io.finished
 }
