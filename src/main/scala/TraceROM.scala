f2ca3ad0aab7b2309525ce0d5a2c7e30b229770b
Howard Mao <zhehao.mao@gmail.com>
2018-07-22 21:46:17 -0700

    add TraceROM for outputting data stream from file

---
+++ src/main/scala/TraceROM.scala
@@ -0,0 +1,16 @@
+package icenet
+
+import chisel3._
+import chisel3.util._
+import testchipip.StreamChannel
+import IceNetConsts.NET_IF_WIDTH
+
+class TraceROM extends BlackBox {
+  val io = IO(new Bundle {
+    val clock = Input(Clock())
+    val reset = Input(Bool())
+    val stream = Decoupled(new StreamChannel(NET_IF_WIDTH))
+    val macAddr = Output(UInt(48.W))
+    val length = Output(UInt(32.W))
+  })
+}
