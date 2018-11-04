550fd1b20dd25847b195e5745c51251d38d8078c
Howard Mao <zhehao.mao@gmail.com>
2018-11-04 02:04:14 +0000

    use rocketchip GeneratorApp instead of testchipip's

--- src/main/scala/TestHarness.scala
+++ src/main/scala/TestHarness.scala
@@ -1,10 +1,12 @@
 package icenet
 
 import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.util.GeneratorApp
 
 class TestHarness(implicit p: Parameters) extends freechips.rocketchip.unittest.TestHarness
 
-object Generator extends testchipip.GeneratorApp {
+object Generator extends GeneratorApp {
+  val longName = names.topModuleProject + "." + names.topModuleClass + "." + names.configs
   generateFirrtl
   generateAnno
 }
