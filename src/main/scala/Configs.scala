ef1035f08e31f0bce10f31e0cdd30ac014ba3322
Howard Mao <zhehao.mao@gmail.com>
2019-01-16 19:54:12 -0800

    remove deprecated switch

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -10,8 +10,6 @@ class WithIceNetUnitTests extends Config((site, here, up) => {
   case UnitTests => (p: Parameters) => {
     Seq(
       Module(new NetworkPacketBufferTest),
-      //Module(new BasicSwitchTestWrapper()(p)),
-      //Module(new BroadcastTestWrapper()(p)),
       Module(new NetworkTapTest),
       Module(new RateLimiterTest),
       Module(new AlignerTest),
