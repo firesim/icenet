705ca50690383aa589dc560a5e7c152af04c46ad
Howard Mao <zhehao.mao@gmail.com>
2020-04-22 14:34:44 -0700

    delete TestHarness

--- src/main/scala/TestHarness.scala
+++
@@ -1,9 +0,0 @@
-package icenet
-
-import freechips.rocketchip.config.Parameters
-import firrtl.options.{StageMain}
-import freechips.rocketchip.system.{RocketChipStage}
-
-class TestHarness(implicit p: Parameters) extends freechips.rocketchip.unittest.TestHarness
-
-object Generator extends StageMain(new RocketChipStage)
